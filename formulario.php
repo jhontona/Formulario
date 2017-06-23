9230bf69f12e64c9325f174935dbf44f2eca0ab4
wilthonP <wilthon-pena95@hotmail.com>
2017-06-23 16:26:58 -0500

    cambio de presentacion en ingles
    cambio de presentacion en ingles

--- formulario.php
+++ formulario.php
@@ -7,16 +7,16 @@
 </head>
 <body>
 	<form action="validar.php" method="post" class="table-bordered col-xs-3 well">
-		<div class="form-group"><label for="nombre">Nombre: </label><input class="form-control" type="text" name="nombre" id="nombre" required placeholder="Tu nombre" /></div>
+		<div class="form-group"><label for="nombre">Name: </label><input class="form-control" type="text" name="nombre" id="nombre" required placeholder="Tu nombre" /></div>
 		<div class="form-group"><label for="email">Email: </label><input class="form-control" type="email" name="email" id="email" required placeholder="tucorreo@gmail.com" /></div>
-		<div class="form-group"><label for="telefono">Telefono:</label><input class="form-control" type="text" name="telefono" id="telefono" required placeholder="telefono: 7777777" /></div>
-		<div class="form-group"><label for="mensaje">Mensaje: </label><textarea class="form-control" required name="mensaje" id="mensaje" placeholder="Tu mensaje aqui" ></textarea></div>
+		<div class="form-group"><label for="telefono">Telephone:</label><input class="form-control" type="text" name="telefono" id="telefono" required placeholder="telefono: 7777777" /></div>
+		<div class="form-group"><label for="mensaje">Message: </label><textarea class="form-control" required name="mensaje" id="mensaje" placeholder="Tu mensaje aqui" ></textarea></div>
 		<?php
 			require_once('recaptchalib.php'); 
 			$pubkey = "6LfBV-wSAAAAAG5eDRP4F8Ytw2la6itXBiLBOBLF"; 
 			echo "<center>" . recaptcha_get_html($pubkey)."</center>"; 
 		?>
-		<input type="submit" value="enviar" class="btn btn-danger btn-lg col-md-offset-5"> 
+		<input type="submit" value="SUBMIT" class="btn btn-danger btn-lg col-md-offset-5"> 
 	</form>
 </body>
 </html>
